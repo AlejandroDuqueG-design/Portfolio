6aa8b7dbf17d24a55b00e4092f5c075588b04d27
Alejandro Duque <alegaduq@gmail.com>
2026-02-19 16:58:52 +0000

    fix: import images, wrap links with react router syntax

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,26 +1,40 @@
 import { Avatar, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
+import logoAD from "@/assets/LogoAD.png";
+import profileimg from "@/assets/img-perfil.jpg";
+import { useEffect } from "react";
 
 function NavBar() {
+  const location = useLocation();
+  console.log("location:", location.pathname);
+
+  useEffect(() => {}, []);
+
   return (
     <Navbar fluid rounded>
       <Link to="/">
         <NavbarBrand>
-          <img src="/src/assets/LogoAD.png" className="mr-3 h-8 sm:h-11" alt="Flowbite React Logo" />
+          <img src={logoAD} className="mr-3 h-8 sm:h-11" alt="Logo Alejandro Duque" />
           <span className="self-center whitespace-nowrap text-l font-semibold dark:text-white">My WebDev Portfolio</span>
         </NavbarBrand>
       </Link>
 
       <div className="flex md:order-2">
         <Link to="/aboutme">
-          <Avatar alt="User settings" img="/src/assets/img-perfil.jpg" rounded />
+          <Avatar alt="Profile" img={profileimg} rounded />
         </Link>
         <NavbarToggle />
       </div>
       <NavbarCollapse>
-        <NavbarLink href="/" active>Home</NavbarLink>
-        <NavbarLink href="/aboutme">About me</NavbarLink>
-        <NavbarLink href="/projects">Projects</NavbarLink>
+        <Link to="/">
+          <NavbarLink active={location.pathname === "/"}>Home</NavbarLink>
+        </Link>
+        <Link to="/aboutme">
+          <NavbarLink active={location.pathname === "/aboutme"}>About me</NavbarLink>
+        </Link>
+        <Link to="/projects">
+          <NavbarLink active={location.pathname === "/projects"}>Projects</NavbarLink>
+        </Link>
       </NavbarCollapse>
     </Navbar>
   );
