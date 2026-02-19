8581307418ac3f9c21283565d283d39f38c308a8
Alejandro Duque <alegaduq@gmail.com>
2026-02-19 16:56:03 +0000

    fix: improve margin and color

--- src/pages/AboutMePage.tsx
+++ src/pages/AboutMePage.tsx
@@ -1,4 +1,4 @@
 function AboutMePage() {
-  return <div className="flex justify-center items-center h-screen bg-violet-950 text-white">About Me Page</div>;
+  return <div className="flex justify-center items-center h-screen bg-indigo-950 text-white">About Me Page</div>;
 }
 export default AboutMePage;
--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,8 +1,8 @@
 function ProjectsPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-violet-950 text-white">
-      <h3 className="text-2xl font-bold">Web Development Projects</h3>
-      <br />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-indigo-950 text-white">
+      <h3 className="text-2xl font-bold mb-5">Web Development Projects</h3>
+
       <p className="text-lg">This section is dedicated to showcase some of the projects I've worked on.</p>
     </div>
   );
